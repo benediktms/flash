4b0722ff52505d90c24f0242bc47bab7a3e9c9ed
Benedikt Schnatterbeck <benediktms@hey.com>
2022-07-01 21:25:48 +0100

    Setup tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import { trpc } from "@/utils/trpc";
 import { Box, Center, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Ben" }]);
+
   return (
     <Center>
       <Box>
         <Heading my={3}>Achieve your goals in a flash.</Heading>
         <Box>Start learning</Box>
+        {isLoading && "Loading"}
+        {data && data.greeting}
       </Box>
     </Center>
   );
