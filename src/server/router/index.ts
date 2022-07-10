4d78a448fd3cacd690404a1f9641e0065d4810bf
Benedikt Schnatterbeck <benediktms@hey.com>
2022-07-10 20:24:49 +0200

    Add pino logger

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,9 +4,27 @@ import { flashCardRouter } from './flashCard';
 import { setRouter } from './set';
 import { userRouter } from './user';
 import { authRouter } from './auth';
+import { logger } from '@/lib/logger';
 
 export const appRouter = createRouter()
   .transformer(superjson)
+  .middleware(async ({ ctx, path, next }) => {
+    const data = {
+      path,
+      request: {
+        method: ctx.req?.method,
+        url: ctx.req?.url,
+        body: ctx.req?.body,
+      },
+      response: {
+        status: ctx.res?.statusCode,
+      },
+    };
+
+    logger.info(data);
+
+    return await next();
+  })
   .merge('auth.', authRouter)
   .merge('user.', userRouter)
   .merge('flashCard.', flashCardRouter)
