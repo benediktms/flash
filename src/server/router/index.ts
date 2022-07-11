8fae15619326597560bc383d9cef7aab74809188
Benedikt Schnatterbeck <benediktms@hey.com>
2022-07-11 10:34:16 +0200

    Implement logging for trpc requests

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,9 +2,27 @@ import { createRouter } from '../context';
 import superjson from 'superjson';
 import { userRouter } from './user';
 import { authRouter } from './auth';
+import { logger } from '@/lib/logger';
 
 export const appRouter = createRouter()
   .transformer(superjson)
+  .middleware(async ({ ctx, path, rawInput, next }) => {
+    const req = ctx.req;
+    const res = ctx.res;
+
+    const data = {
+      request: {
+        path,
+        url: req?.url,
+        body: rawInput,
+      },
+      response: { status: res?.statusCode },
+    };
+
+    logger.info(data);
+
+    return await next();
+  })
   .merge('auth.', authRouter)
   .merge('user.', userRouter);
 
